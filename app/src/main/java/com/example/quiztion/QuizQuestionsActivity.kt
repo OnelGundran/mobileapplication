87fa5d8a13161f42e0c7b99ac4e0438361ce7f7c
HP <abdullhazuber@gmail.com>
2020-10-26 23:37:48 +0800

    home Screen

--- app/src/main/java/com/example/quiztion/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quiztion/QuizQuestionsActivity.kt
@@ -3,13 +3,11 @@ package com.example.quiztion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_quiz_questions)
     }
-
 
 
 }
